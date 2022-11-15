18699b450c7af5d9ecaa357885562b312ccf7082
Czar272 <110710742+Czar272@users.noreply.github.com>
2022-11-14 22:38:55 -0600

    Add files via upload

---
+++ View/VisualizacionPerfiles/src/Visualizador_de_Perfil.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Visualizador_de_Perfil {
+    private JPanel panel1;
+    private JButton REGRESARButton;
+    private JButton rechazarButton;
+    private JButton aceptarButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
