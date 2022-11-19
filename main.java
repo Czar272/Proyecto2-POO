fd608c726ee80b841db725436b5ce52b681bc362
MarchMol <98711734+MarchMol@users.noreply.github.com>
2022-11-18 18:53:30 -0600

    revision de archivos
    a

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import Proyecto2.Archivo;
 import Proyecto2.DatosActuales;
 import Proyecto2.Perfil;
+import Proyecto2.View.PantallaEdicion;
 
 public class main {
     public static void main(String[] args) {
@@ -10,6 +11,13 @@ public class main {
         dat.ArPerfiles = ar.FormatAbrir();
         for (Perfil p : dat.ArPerfiles) {
             System.out.println(p.getNombre());
+            System.out.println(p.getDescripcion());
+            System.out.println("URL: " + p.getImagen());
+            System.out.println("Intereses:");
+            for (String st : p.getIntereses()) {
+                System.out.println("    " + st);
+            }
+            System.out.println("");
         }
     }
 }
